6fd59cfa61c670cc8dd5c1cd6080fb7e05e4ae87
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-23 09:26:43 +0300

    3-hypermedia_del_pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""3-hypermedia_del_pagination.py"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Method with two integer arguments: index with a None default value and
+        page_size with default value of 10
+        """
+        sorted_ds = self.indexed_dataset()
+        assert isinstance(index, int)
+        assert index >= 0
+        assert index < len(self.__indexed_dataset)
+        data = [sorted_ds.get(i) for i in range(index, index + page_size)]
+        return {
+            'index': index,
+            'next_index': index + 1,
+            'page_size': len(data),
+            'data': data
+        }
