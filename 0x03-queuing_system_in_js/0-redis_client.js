63ec4cb120f3db556870b7c7f5cf199dc876d0bf
Ernest Wambua <ernestwambua2@gmail.com>
2023-02-27 10:04:56 +0300

    1-redis_op

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,17 +1,12 @@
-import { createClient } from 'redis';
+import redis from "redis";
 
 // create a redis client
-const client = createClient({ url: 'redis://localhost:6379' });
+const client = redis.createClient({ url: "redis://localhost:6379" });
 
 // error handler incase client fails to connect
-client.on('error', (err) =>
-	console.log(`Redis client not connected to the server: ${err.message}`)
+client.on("error", (err) =>
+  console.log(`Redis client not connected to the server: ${err.message}`)
 );
 
 // handle connection event
-client.on('connect', () => console.log('Redis client connected to the server'));
-
-// connect to the redis server
-(async () => {
-	await client.connect();
-})();
+client.on("connect", () => console.log("Redis client connected to the server"));
